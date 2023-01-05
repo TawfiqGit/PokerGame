3f36c2a6f25ee94126dcc811e12f5d8c85debc79
TawfiqGit <mtawfiq95@gmail.com>
2023-01-05 14:19:48 +0100

    add all class

---
+++ PokerGame/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  PokerGame
+//
+//  Created by admin on 05/01/2023.
+//
+
+import Foundation
+
+struct Player {
+    
+    var name : String = ""
+    var main : Int = 0
+    var jeton : Int = 0
+
+}
---
+++ PokerGame/ViewModel/BoardViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BoardViewModel.swift
+//  PokerGame
+//
+//  Created by admin on 05/01/2023.
+//
+
+import Foundation
+
+class BoardViewModel : NSObject{
+  
+}
